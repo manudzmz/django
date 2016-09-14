9d5c54a2c76cfafb939c92e9fbac977ae1d3ed4a
Manuel <manudzmz@gmail.com>
2016-09-15 01:45:03 +0200

    Modificado el modelo photos para añadir usuario y visibilidad.

--- photos/models.py
+++ photos/models.py
@@ -1,20 +1,34 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+LICENSE_COPYRIGHT = 'RIG'
+LICENSE_COPYLEFT = 'LEF'
+LICENSE_CC = 'CC'
+LICENSES = (
+    (LICENSE_COPYRIGHT, 'Copyright'),
+    (LICENSE_COPYLEFT, 'Copyleft'),
+    (LICENSE_CC, 'Creative Commons')
+)
+
+VISIBILITY_PUBLIC = 'PUB'
+VISIBILITY_PRIVATE = 'PRI'
+
+VISIBILITY = {
+    (VISIBILITY_PUBLIC, 'Pública'),
+    (VISIBILITY_PRIVATE, 'Privada')
+}
 
 
 # Create your models here.
 class Photo(models.Model):
-    LICENSES = (
-        ('RIG', 'Copyright'),
-        ('LEF', 'Copyleft'),
-        ('CC', 'Creative Commons')
-    )
-
+    owner = models.ForeignKey(User)
     name = models.CharField(max_length=150)
     url = models.URLField()
-    description = models.TextField()
-    license = models.CharField(max_length=3, choices=LICENSES)
+    description = models.TextField(null=True, blank=True)
+    license = models.CharField(max_length=3, choices=LICENSES, default=LICENSE_CC)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+    visibility = models.CharField(max_length=3, choices=VISIBILITY, default=VISIBILITY_PUBLIC)
 
     def __str__(self):  # mifoto.__str__()
         return self.name
