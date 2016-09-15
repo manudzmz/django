8bdbc2ad61c8ffc311bad309acdba942b31c6201
Manuel <manudzmz@gmail.com>
2016-09-16 00:58:37 +0200

    Implementamos el detalle de foto

--- frikr/urls.py
+++ frikr/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from photos.views import home
+from photos.views import home, photo_detail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home),
+    url(r'^photos/(?P<pk>[0-9]+)$', photo_detail),
+    url(r'^$', home)
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from photos.models import Photo, VISIBILITY_PUBLIC
 
@@ -14,3 +14,21 @@ def home(request):
     photos = Photo.objects.filter(visibility=VISIBILITY_PUBLIC).order_by('-created')
     context = {'photos_list': photos[:4]}
     return render(request, "photos/home.html", context)
+
+
+def photo_detail(request, pk):
+    """
+    Renderiza el detalle de una imagen
+    :param request: objeto HttpRequest con los datos de la peticion
+    :param pk: clave primaria de la foto a recuperar
+    :return: objeto HttpResponse con los datos de la respuesta
+    """
+    possible_photos = Photo.objects.filter(pk=pk)
+    if len(possible_photos) == 0:
+        return HttpResponseNotFound("La imagen que buscas no existe")
+    elif len(possible_photos) > 1:
+        return HttpResponse("Múltiples opciones", status=300)
+
+    photo = possible_photos[0]
+    context = {'photo': photo}
+    return render(request, 'photos/photo_detail.html', context)
