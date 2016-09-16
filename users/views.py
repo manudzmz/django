4f54799e42f8e19571535a6e19a179e42de7f4d7
Manuel <manudzmz@gmail.com>
2016-09-16 19:13:23 +0200

    Creacion de formulario con datos rellenos o no dependiendo del metodo de envio. Seleccion de datos con cleaned_data en lugar de request.POST

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.shortcuts import render, redirect
-
+from users.forms import LoginForm
 
 # Create your views here.
+
+
+
 def login(request):
     """
     Presenta el formulario de login y gestiona el login de un usuario
@@ -10,21 +13,25 @@ def login(request):
     :return: objeto HttpResponse con los datos de la respuesta
     """
     error_message = ""
+    # request.POST rellena los campos del formulario
+    login_form = LoginForm(request.POST) if request.method == "POST" else LoginForm()
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('pwd')
-        user = authenticate(username=username, password=password)
-        if user is None:
-            error_message = "Usuario o contraseña incorrecto"
-        else:
-            if user.is_active:
-                # decimos al request que usuario esta autenticado para futuras peticiones
-                django_login(request, user)
-                return redirect('/')
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('pwd')
+            user = authenticate(username=username, password=password)
+            if user is None:
+                error_message = "Usuario o contraseña incorrecto"
             else:
-                error_message = "Cuenta de usuario inactiva"
+                if user.is_active:
+                    # decimos al request que usuario esta autenticado para futuras peticiones
+                    django_login(request, user)
+                    return redirect('/')
+                else:
+                    error_message = "Cuenta de usuario inactiva"
 
-    return render(request, 'users/login.html', {'error': error_message})
+    context = {'error': error_message, 'form': login_form}
+    return render(request, 'users/login.html', context)
 
 
 def logout(request):
