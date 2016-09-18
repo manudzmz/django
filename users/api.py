36fa1a22b43a9216152190f90990e7a98ff3d243
Manuel <manudzmz@gmail.com>
2016-09-19 01:04:03 +0200

    Creamos endpoint de detalle de usuario, endpoint de actualizacion de usuario y dividimos el serializador en uno para post y otro para get.

--- users/api.py
+++ users/api.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
-from users.serializers import UserSerializer
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_202_ACCEPTED
+from users.serializers import UserSerializer, UserListSerializer
 
 
 class UserListAPI(APIView):
@@ -11,7 +12,7 @@ class UserListAPI(APIView):
     """
     def get(self, request):
         users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        serializer = UserListSerializer(users, many=True)
         # many=True convierte la lista de usuarios en una lista de diccionarios
         return Response(serializer.data)
 
@@ -22,3 +23,23 @@ class UserListAPI(APIView):
             return Response(serializer.data, status=HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPI(APIView):
+    """
+    Endpoint del detalle de un usuario
+    """
+    def get(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
