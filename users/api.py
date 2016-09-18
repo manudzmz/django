9455971794866d9a0f2e1c36639f61f90977e2e8
Manuel <manudzmz@gmail.com>
2016-09-18 23:45:01 +0200

    Creamos el endpoint de un usuario a falta de encriptar su contraseña para que pueda autenticarse

--- users/api.py
+++ users/api.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from users.serializers import UserSerializer
 
 
@@ -15,3 +14,11 @@ class UserListAPI(APIView):
         serializer = UserSerializer(users, many=True)
         # many=True convierte la lista de usuarios en una lista de diccionarios
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
