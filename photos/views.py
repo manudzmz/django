2de220db834039f5206440a1216dd96310eda760
Manuel <manudzmz@gmail.com>
2016-09-18 03:30:33 +0200

    Corregimos error de seguridad que permitia ver fotos privadas a usuarios no autorizados.

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
@@ -36,6 +37,10 @@ class PhotoDetailView(View):
         :return: objeto HttpResponse con los datos de la respuesta
         """
         possible_photos = Photo.objects.filter(pk=pk).select_related("owner")
+        if not request.user.is_authenticated():
+            possible_photos = possible_photos.filter(visibility=VISIBILITY_PUBLIC)
+        else:
+            possible_photos = possible_photos.filter(Q(visibility=VISIBILITY_PUBLIC) | Q(owner=request.user))
         if len(possible_photos) == 0:
             return HttpResponseNotFound("La imagen que buscas no existe")
         elif len(possible_photos) > 1:
