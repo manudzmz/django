7a401942ae58af8d3b8b8d79ecbb560fe549af6b
Manuel <manudzmz@gmail.com>
2016-09-19 14:27:44 +0200

    API de fotos terminada.

--- photos/api.py
+++ photos/api.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from photos.models import Photo
-from photos.serializers import PhotoSerializer
+from photos.serializers import PhotoSerializer, PhotoListSerializer
 
 
 class PhotoListAPI(ListCreateAPIView):
@@ -9,7 +9,9 @@ class PhotoListAPI(ListCreateAPIView):
     Endpoint de listado y creación de fotos
     """
     queryset = Photo.objects.all()
-    serializer_class = PhotoSerializer
+
+    def get_serializer_class(self):
+        return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
 
 
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
