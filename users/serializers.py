7096a3632813e3314279fba6ac1479d9e123f092
Manuel <manudzmz@gmail.com>
2016-09-19 15:53:43 +0200

    Implementada validacion de nombres de usuario y de email.

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class UserListSerializer(serializers.Serializer):
@@ -30,4 +31,15 @@ class UserSerializer(serializers.Serializer):
         instance.save()
         return instance
 
+    def validate_username(self, username):
+        username = super().validate_username(username)
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("El nombre de usuario {0} ya está siendo utilizado".format(username))
+        return username
 
+    def validate_email(self, email):
+        email = super().validate_email(email)
+        email = super().validate_email(email)
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("El e-mail {0} ya está siendo utilizado".format(email))
+        return email.lower()
